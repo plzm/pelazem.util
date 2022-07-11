da16c1f5f3b86f0dcd00098087f5716948c48979
Patrick El-Azem <p@elazem.com>
2022-07-11 06:26:34 -0400

    Remove useless throws:

--- src/pelazem.util/ErrorUtil.cs
+++ src/pelazem.util/ErrorUtil.cs
@@ -13,28 +13,21 @@ namespace pelazem.util
 
 			string result;
 
-			try
+			StringBuilder sb = new();
+
+			sb.AppendLine(Properties.Resources.Type + ": " + ex.GetType().Name);
+			sb.AppendLine(Properties.Resources.Source + ": " + ex.Source);
+			sb.AppendLine(Properties.Resources.Message + ": " + ex.Message);
+			sb.AppendLine(Properties.Resources.StackTrace + ":");
+			sb.AppendLine(ex.StackTrace);
+
+			if (ex.InnerException != null)
 			{
-				StringBuilder sb = new();
-
-				sb.AppendLine(Properties.Resources.Type + ": " + ex.GetType().Name);
-				sb.AppendLine(Properties.Resources.Source + ": " + ex.Source);
-				sb.AppendLine(Properties.Resources.Message + ": " + ex.Message);
-				sb.AppendLine(Properties.Resources.StackTrace + ":");
-				sb.AppendLine(ex.StackTrace);
-
-				if (ex.InnerException != null)
-				{
-					sb.AppendLine(Properties.Resources.InnerException + ":");
-					sb.AppendLine(GetText(ex.InnerException));
-				}
-
-				result = sb.ToString();
+				sb.AppendLine(Properties.Resources.InnerException + ":");
+				sb.AppendLine(GetText(ex.InnerException));
 			}
-			catch
-			{
-				result = Properties.Resources.ErrorMsgCouldNotGetExceptionText;
-			}
+
+			result = sb.ToString();
 
 			return result;
 		}
@@ -46,18 +39,11 @@ namespace pelazem.util
 
 			string result;
 
-			try
-			{
-				result =
-					Properties.Resources.Type + ": " + errorObject.GetType().Name + Environment.NewLine +
-					Properties.Resources.Text + ": " + Environment.NewLine + errorObject.ToString() + Environment.NewLine +
-					Properties.Resources.StackTrace + ":" + Environment.NewLine +
-					Environment.StackTrace + Environment.NewLine;
-			}
-			catch
-			{
-				result = Properties.Resources.ErrorMsgCouldNotGetErrorText;
-			}
+			result =
+				Properties.Resources.Type + ": " + errorObject.GetType().Name + Environment.NewLine +
+				Properties.Resources.Text + ": " + Environment.NewLine + errorObject.ToString() + Environment.NewLine +
+				Properties.Resources.StackTrace + ":" + Environment.NewLine +
+				Environment.StackTrace + Environment.NewLine;
 
 			return result;
 		}
